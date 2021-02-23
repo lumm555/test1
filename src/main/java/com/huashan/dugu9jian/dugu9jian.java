1e145a3e131a23a0fe6e4fd1251ba9838824bfea
lumm555 <307055607@qq.com>
2021-02-23 22:32:10 +0800

    独孤九剑心法

---
+++ src/main/java/com/huashan/dugu9jian/dugu9jian.java
@@ -0,0 +1,9 @@
+package com.huashan.dugu9jian;
+
+public class dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("dugu9jian hao");
+        System.out.println("dugu9jian hao");
+        System.out.println("dugu9jian hao");
+    }
+}
